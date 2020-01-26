c848f0aec66804633926024234fcb82a5143e6cc
SahilDhull <sahildhull25121998@gmail.com>
2020-01-26 13:47:48 +0530

    vel,acc to throttle map

---
+++ throttle_acc.py
@@ -0,0 +1,25 @@
+# For TeslaModel3
+
+v_a_to_th = []
+
+# v = 0
+a_to_th = {}
+v_a_to_th.append(a_to_th)
+
+# v = 1
+a_to_th = {0.78:0.1, 1.6:0.2, 2.45:0.3}
+v_a_to_th.append(a_to_th)
+
+# v = 2
+a_to_th = {0.73:0.1, 1.55:0.2, 2.4:0.3}
+v_a_to_th.append(a_to_th)
+
+# v = 3
+a_to_th = {0.68:0.1, 1.51:0.2, 2.35:0.3}
+v_a_to_th.append(a_to_th)
+
+# v = 4
+a_to_th = {0.63:1, 1.46:0.2, 2.3:0.3}
+v_a_to_th.append(a_to_th)
+
+print (v_a_to_th[2][2.4])
