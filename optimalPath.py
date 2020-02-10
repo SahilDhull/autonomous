0e435ebce33fab0c6f2a6a6ef51973458c5a0003
jatinjindal <jatinjindal369@gmail.com>
2020-02-10 18:28:02 +0530

    set (a,v)

--- optimalPath.py
+++ optimalPath.py
@@ -4,21 +4,53 @@ import math
 import copy
 import dubins
 import shapely.geometry as geom
-import threading
+import threading    
 from statistics import median 
+
+
 
 
 #Change radius of curvature for 0.9
 from vel_acc_to_throttle import *
 
+
+lock = threading.Lock()
 inf = 1e9
 No_of_threads = 11
-<<<<<<< HEAD
-acc= [-10.0,-5.0,-3.0,-1.0,0.01,1.0,2.0]
-=======
 
-acc= [-10.0,-5.0, -3.0, -1.0, 0.01, 1.0, 2.0]
->>>>>>> a13780131da8182a9231a1fb06551aeef32056d5
+acc= {}
+acc[0] = [-1.0, -0.5, 0.5, 1.0, 2.0, 4.0]
+acc[1] = [-1.0, -0.5, 0.5, 1.0, 2.0, 4.0]
+acc[2] = [-1.0, -0.5, 0.5, 1.0, 2.0, 4.0]
+acc[3] = [-1.0, -0.5, 0.5, 1.0, 2.0, 4.0]
+acc[4] = [-1.0, -0.5, 0.5, 1.0, 2.0, 4.0]
+acc[5] = [-1.0, 0.0, 1.0, 2.0, 4.0]
+acc[6] = [-1.0, 0.0, 1.0, 2.0, 4.0]
+acc[7] = [-1.0, 0.0, 1.0, 2.0, 4.0]
+acc[8] = [-1.0, 0.0, 1.0, 2.0, 4.0]
+acc[9] =  [-3.0, -1.0, 0.0, 1.0, 2.0, 4.0]
+acc[10] = [-3.0, -1.0, 0.0, 1.0, 2.0, 4.0]
+acc[11] = [-3.0, -1.0, 0.0, 1.0, 2.0, 4.0]
+acc[12] = [-3.0, -1.0, 0.0, 1.0, 2.0, 4.0]
+acc[13] = [-3.0, -1.0, 0.0, 1.0, 2.0, 4.0]
+acc[14] = [-3.0, -1.0, 0.0, 1.0, 2.0, 4.0]
+acc[15] = [-3.0, -1.0, 0.0, 1.0, 2.0, 4.0]
+acc[16] = [-5.0, -3.0, -1.0, 0.0, 1.5, 3.0]
+acc[17] = [-5.0, -3.0, -1.0, 0.0, 1.5, 3.0]
+acc[18] = [-5.0, -3.0, -1.0, 0.0, 1.5, 3.0]
+acc[19] = [-5.0, -3.0, -1.0, 0.0, 1.5, 3.0]
+acc[20] = [-5.0, -3.0, -1.0, 0.0, 1.5, 3.0]
+acc[21] = [-5.0, -3.0, -1.0, 0.0, 1.5]
+acc[22] = [-5.0, -3.0, -1.2, 0.0, 1.5]
+acc[23] = [-5.0, -3.0, -1.2, 0.0, 1.0]
+acc[24] = [-5.0, -3.0, -1.2, 0.0, 1.0]
+acc[25] = [-5.0, -3.0, -1.2, 0.0, 1.0]
+acc[26] = [-5.0, -3.0, -1.3, 0.0, 1.0]
+acc[27] = [-5.0, -3.0, -1.4, 0.0, 1.0]
+acc[28] = [-5.0, -3.0, -1.4, 0.0, 0.5]
+acc[29] = [-5.0, -3.0, -1.4, 0.0, 0.5]
+acc[30] = [-5.0, -3.0, -1.5, 0.0]
+
 
 total_distance = 150.0
 grid_points = []
@@ -38,18 +70,21 @@ temp_vel = {}
 temp_acc = {}
 temp_c = {}
 temp_p = {}
+
+temp_theta = {}
+cur_theta = {}
 #-----------------------------------------
 
 
-y_step = 0.9
+y_step = 0.4
 x_step = 5.0
 w = 3.6
 obs_initial_pos = [450.0,0.0]
 obs_vel = 5.0
-corner_local_coords = [[-1.1, 2.5], [1.1, 2.5], [-1.1, -2.5], [1.1, -2.5]]
+corner_local_coords = [[-2.5, 1.1], [-2.5, -1.1], [2.5, 1.1], [2.5, -1.1]]
 Radius_of_road = 20.0
     
-def RadiusofCurvature(start_pt, end_pt, turn_radius=20.0, step_size=1.0):
+def RadiusofCurvature(start_pt, end_pt, turn_radius=30.0, step_size=1.0):
     """Generate points along a Dubins path connecting start point to end point.
     Format for input / output points: (x, y, angle)"""
     min_turn_radius = min(0.1, turn_radius)
@@ -68,7 +103,7 @@ def RadiusofCurvature(start_pt, end_pt, turn_radius=20.0, step_size=1.0):
         if cex_found:
             # Decrease radius until finding a satisfying result.
             # We could do a binary search but that requires a termination condition.
-            turn_radius = turn_radius*0.9
+            turn_radius = turn_radius*0.8
             if turn_radius < min_turn_radius:
                 break
     if not satisfied:
@@ -76,16 +111,31 @@ def RadiusofCurvature(start_pt, end_pt, turn_radius=20.0, step_size=1.0):
     return turn_radius
 
 
-def rotate_point_ccw(point, theta):
+def rotate_point_cw(point, theta):
     cos_theta = math.cos(theta)
     sin_theta = math.sin(theta)
-    return np.dot(np.array([[cos_theta, -sin_theta], [sin_theta, cos_theta]]), point)
+    return np.dot(np.array([[cos_theta, sin_theta], [-sin_theta, cos_theta]]), point)
+
+def ObsPosition(t):
+    Total_time = (1000.0 + 2*math.pi*20.0)/obs_vel
+    t = t - Total_time * int(t/Total_time)
+    offset = t * obs_vel
+    if( obs_initial_pos[0] - offset >=0):
+        return [obs_initial_pos[0]-offset,obs_initial_pos[1]]
+    elif( obs_initial_pos[0] - (offset - math.pi * Radius_of_road) >=0 ):
+        turned_theta = (offset - obs_initial_pos[0])/Radius_of_road
+
 
 def check_colliding(pt2):
+    turned = pt2[0][2]-pt2[4]
     obstacle_position = [obs_initial_pos[0] - obs_vel*pt2[3],obs_initial_pos[1]]
     car_corner_pos = []
     for local_coord in corner_local_coords:
-        car_corner_pos.append([pt2[0][0]+local_coord[1],pt2[0][1]+local_coord[0]])
+        rotated_local_coord = \
+            rotate_point_cw(point=np.transpose(np.array(local_coord)),
+                             theta=turned)
+        
+        car_corner_pos.append([pt2[0][0]+rotated_local_coord[0],pt2[0][1]+rotated_local_coord[1]])
 
 
     obs_corner_pos = []
@@ -94,18 +144,23 @@ def check_colliding(pt2):
         #     rotate_point_ccw(point=np.transpose(np.array(local_coord)),
         #                      rotation_angle=-detected_objects[obj_ind].object_yaw_angle)
         rotated_local_coord = \
-            rotate_point_ccw(point=np.transpose(np.array(local_coord)),
+            rotate_point_cw(point=np.transpose(np.array(local_coord)),
                              theta=0.0)
         
-        obs_corner_pos.append([obstacle_position[0] + rotated_local_coord[1],
-                             obstacle_position[1] + rotated_local_coord[0]])
+        obs_corner_pos.append([obstacle_position[0] + rotated_local_coord[0],
+                             obstacle_position[1] + rotated_local_coord[1]])
 
 
     collision = 0
     for pos in car_corner_pos:
-        if (pos[0]>=obs_corner_pos[2][0] and pos[0]<=obs_corner_pos[0][0] and pos[1]<=obs_corner_pos[1][1] and pos[1]>=obs_corner_pos[0][1]): 
+        # pos[0] = pos[0] - pt2[2]*2
+        if (pos[0]>=obs_corner_pos[0][0] and pos[0]<=obs_corner_pos[2][0] and pos[1]<=obs_corner_pos[0][1] and pos[1]>=obs_corner_pos[1][1]): 
             collision=1
             break
+        # pos[0] = pos[0] + 3*pt2[2]
+        # if (pos[0]>=obs_corner_pos[0][0] and pos[0]<=obs_corner_pos[2][0] and pos[1]<=obs_corner_pos[0][1] and pos[1]>=obs_corner_pos[1][1]): 
+        #     collision=1
+        #     break
     return collision    
 
 def cost(c1, pt1,pt2, off=0.0):
@@ -122,17 +177,17 @@ def cost(c1, pt1,pt2, off=0.0):
     if(temp in R):
         r = R[temp]
     else:
-        r = RadiusofCurvature(pt1[0],pt2[0])
-        if(r>=20):
+        r = RadiusofCurvature([pt1[0][0],pt1[0][1],pt1[4]],[pt2[0][0],pt2[0][1],pt2[4]])
+        if(r==30):
             r=inf
         R[temp] = r
 
-    static_cost =  c1 + math.sqrt((pt2[0][0]-pt1[0][0])**2 + (pt2[0][1]-pt1[0][1])**2) + 10.0/r + 20.0*abs(off)
-<<<<<<< HEAD
-    dynamic_cost = 100*(pt2[3]-pt1[3]) + (pt2[2]**2)*0.0 + 0.0*(pt2[1]**2) + 1.0*(((pt2[1]-pt1[1])/(pt2[3]-pt1[3]))**2) + 10*(((pt2[2])**2)/r)
-=======
-    dynamic_cost = 15*(pt2[3]-pt1[3]) + (pt2[2]**2)*0.0 + 0.0*(pt2[1]**2) + 1.0*(((pt2[1]-pt1[1])/(pt2[3]-pt1[3]))**2) + 10*(((pt2[2])**2)/r)
->>>>>>> a13780131da8182a9231a1fb06551aeef32056d5
+
+    obstacle_position = [obs_initial_pos[0] - obs_vel*pt2[3],obs_initial_pos[1]]
+    
+    static_cost =  c1 + math.sqrt((pt2[0][0]-pt1[0][0])**2 + (pt2[0][1]-pt1[0][1])**2) + 10.0/r + 10.0*abs(off) + 0.1*math.exp(-0.1*math.sqrt((pt2[0][0]-obstacle_position[0])**2 + (pt2[0][1]-obstacle_position[1])**2))
+
+    dynamic_cost = 50*(pt2[3]-pt1[3]) + (pt2[2]**2)*0.0 + 0.0*(pt2[1]**2) + 1.0*(((pt2[1]-pt1[1])/(pt2[3]-pt1[3]))**2) + 0.5*(((pt2[2])**2)/r)
     
     return static_cost + dynamic_cost + check_colliding(pt2)*inf
 
@@ -232,6 +287,7 @@ def computeTargetPath(cur_pt, dist_to_cover):
     global actual_vel
     global actual_tim
     global prev_acc
+    global cur_theta
        
     # print(grid_points)
 
@@ -257,7 +313,7 @@ def computeTargetPath(cur_pt, dist_to_cover):
     actual_tim[(0,ind2,i3,i4)] = cur_pt[4]
     actual_vel[(0,ind2,i3,i4)] = cur_pt[3]
     prev_acc[(0,ind2,i3,i4)] = cur_pt[2]
-
+    cur_theta[(ind2,i3,i4)] = cur_pt[5]
 
     global velocities
     global times
@@ -266,6 +322,8 @@ def computeTargetPath(cur_pt, dist_to_cover):
     global temp_tim
     global temp_p
     global temp_acc
+    global temp_theta
+
 
     cf = inf
     final_pos = -1
@@ -334,6 +392,7 @@ def computeTargetPath(cur_pt, dist_to_cover):
                 actual_vel[(i+1,j,ind_v,ind_t)] = temp_vel[(i+1,j,v,t)]
                 actual_tim[(i+1,j,ind_v,ind_t)] = temp_tim[(i+1,j,v,t)]
                 prev_acc[(i+1,j,ind_v,ind_t)] = temp_acc[(i+1,j,v,t)]
+                cur_theta[(j,ind_v,ind_t)] = temp_theta[(j,v,t)]
                 if(i==Y-2) and (cf>c[(j,ind_v,ind_t)]):
                     cf = c[(j,ind_v,ind_t)]
                     final_pos = (i+1,j,ind_v,ind_t)
@@ -348,6 +407,7 @@ def computeTargetPath(cur_pt, dist_to_cover):
         temp_acc = {}
         temp_p = {}
         temp_tim = {}
+        temp_theta = {}
 
 
 
@@ -375,22 +435,25 @@ def parallel_func(ind4,i,X):
     global temp_acc
     global temp_vel
     global temp_tim
+    global temp_theta
 
     global velocities
     global times
-    while(ind4 < len(acc)):
-        for (j,ind2,ind3) in c:
-            m1 = max(0,j-2)
-            m2 = min(X-1,j+2)
+    for (j,ind2,ind3) in c:
+        v_i = math.ceil(actual_vel[(i,j,ind2,ind3)])
+        if(ind4 < len(acc[v_i])):
+            m1 = max(0,j-3)
+            m2 = min(X-1,j+3)
             for k in range(m1,m2+1):
-                a_f = acc[ind4]
+                a_f = acc[ind2][ind4]
                 cur_cost = 0
                 v_f = ( (actual_vel[(i,j,ind2,ind3)]**2) +2*a_f*x_step)
                 if(v_f < 0):
-                    v_f =0
                     continue
                 else:
                     v_f = v_f ** 0.5
+                if(v_f > 30):
+                    continue
                 v_f = round(v_f,4)
                 
                 ind5 = math.ceil(v_f)
@@ -401,42 +464,61 @@ def parallel_func(ind4,i,X):
                 t_f = round(t_f,2)         
                 ind6 = math.ceil(t_f)
                 
+                x1 = grid_points[i][j][0]
+                y1 = grid_points[i][j][1]
+                x2 = grid_points[i+1][k][0]
+                y2 = grid_points[i+1][k][1]
+                if(x2 < x1):
+                    if(y2 >= y1):
+                        curtheta = math.pi + math.atan((y2-y1)/(x2-x1))
+                    else:
+                        curtheta = math.pi + math.atan((y2-y1)/(x2-x1))
+                elif(x2 > x1):
+                    if(y2 >= y1):
+                        curtheta = math.atan((y2-y1)/(x2-x1))
+                    else:
+                        curtheta = 2*math.pi + math.atan((y2-y1)/(x2-x1))
+                else:
+                    if(y2>y1):
+                        curtheta = math.pi/2.0
+                    else:
+                        curtheta = 1.5*math.pi
+
+
+
+                # curtheta = grid_points[i+1][k][2] - math.atan((k-j)*y_step/x_step)
                 
-                cur_cost = cost(c[(j,ind2,ind3)],(grid_points[i][j],prev_acc[(i,j,ind2,ind3)],actual_vel[(i,j,ind2,ind3)],actual_tim[(i,j,ind2,ind3)]),(grid_points[i+1][k],a_f,v_f,t_f),off=abs(w-k*y_step))
+                cur_cost = cost(c[(j,ind2,ind3)],(grid_points[i][j],prev_acc[(i,j,ind2,ind3)],actual_vel[(i,j,ind2,ind3)],actual_tim[(i,j,ind2,ind3)], cur_theta[(j,ind2,ind3)]),(grid_points[i+1][k],a_f,v_f,t_f,curtheta),off=abs(w-k*y_step))
                 if(cur_cost > inf):
                     continue
                 
+                lock.acquire()
                 if( (k,ind5,ind6) not in temp_c) or (temp_c[(k,ind5,ind6)] > cur_cost):
                     temp_tim[(i+1,k,ind5,ind6)] = t_f
                     temp_c[(k,ind5,ind6)] = cur_cost
                     temp_vel[(i+1,k,ind5,ind6)] = v_f
                     temp_acc[(i+1,k,ind5,ind6)] = a_f
                     temp_p[(i+1,k,ind5,ind6)] = (i,j,ind2,ind3)
+                    temp_theta[(k,ind5,ind6)] = curtheta
                     velocities.append(v_f)
                     times.append(t_f)
+                lock.release()
                 
-
-        ind4+=No_of_threads
 
 
 
 
 
 total_distance_covered = 0
-# cur_pt = [16.77,0.0,0.5,34.45,26.0]
-cur_pt =  [500.0, 0.0, 0.0, 0.0, 0.0]
-<<<<<<< HEAD
-# cur_pt = [80.0,0.0,2.0,21.91,10.97]
-=======
-# cur_pt = [80.0, 0.0, 2.0, 21.9088, 10.97]
+# cur_pt = [16.77,0.0,0.5,34.45,26.0, math.pi]
+cur_pt =  [500.0, 0.0, 0.0, 0.0, 0.0, math.pi]
 
->>>>>>> a13780131da8182a9231a1fb06551aeef32056d5
 path = [cur_pt]
 while(total_distance_covered < 100):
     path = path + computeTargetPath(cur_pt,300)
     # print("path=====================")
     # print(path)
-    total_distance_covered = 150 + total_distance_covered
+    total_distance_covered = 100 + total_distance_covered
     cur_pt = path[-1]
     actual_vel = {}
     actual_tim = {}
@@ -457,6 +539,7 @@ t = []
 # a= []
 throttle = []
 prev = -1
+
 
 for i in output:
     target_path.append([i[0],i[1]])
@@ -479,3 +562,8 @@ print(target_path)
 print(" ")
 print(t)
 
+
+
+
+# r= RadiusofCurvature([5.0,0.0,math.pi],[0.0,0.25,math.pi])
+# print(r)
